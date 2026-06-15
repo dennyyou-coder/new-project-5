6a21aaca5f3d1aa67cd3b41ccebe668b0e087c60
dennyyou-coder <denny.you@gmail.com>
2026-06-15 16:50:18 +0800

    Publish commercial cleaning robot buyer guide

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -15,6 +15,11 @@ type Props = {
 
 const siteUrl = "https://worldcleanbiz.com";
 const relatedArticleOverrides: Record<string, string[]> = {
+  "commercial-cleaning-robot-manufacturers-china": [
+    "how-to-find-reliable-cleaning-product-suppliers-in-china",
+    "floor-washer-manufacturers-china",
+    "robot-vacuum-cleaners-from-china"
+  ],
   "robotic-lawn-mower-manufacturers-china": [
     "how-to-find-reliable-cleaning-product-suppliers-in-china",
     "robot-vacuum-cleaners-from-china",
